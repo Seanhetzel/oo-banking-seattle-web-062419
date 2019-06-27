ec101e17f6412cdf8f5e3ac2c06cf747ff74b6d3
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-27 22:58:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if status == "pending"
+    if status == "pending" && sender.valid?
       sender.balance -= @amount
       receiver.balance += @amount
       @status = "complete"
